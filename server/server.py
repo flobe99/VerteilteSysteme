36799b34ed9b83d00910930fd4792c85c38f2a5c
DanielSoftware <walser.daniel2000@gmail.com>
2021-05-20 14:05:57 +0200

    updated

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@
 from flask import Flask, request
 import sys
 import time
-from datenbank import Database
+from database import Database
 
 app = Flask(__name__)
 
@@ -22,7 +22,10 @@ def createBlackboard(pName, pValidity):
 
     ret = Database.create_blackboard(name,validity,time.time())
 
-    return 'Blackboard updated successfully',200
+	if ret == 200:
+	    return 'Blackboard updated successfully', ret
+	else:
+		return 'An error occurred', ret
 
 @app.route('/blackboard/display', methods=['GET'])      #update Data from the blackboard
 def displayBlackboard():
@@ -32,20 +35,31 @@ def displayBlackboard():
 
     ret = Database.display_blackboard(name,data,None, time.time())
     
-    return 'DISPLAY_BLACKBOARD',200
+	if ret == 200:
+	    return 'DISPLAY_BLACKBOARD', ret
+	else:
+		return 'An error occurred', ret
 
 @app.route('/blackboard/clear', methods=['GET'])        #clear content from the blackboard
 def clearBlackboard():
     #parameters
     name = request.args.get('name')
     ret=Database.clear_blackboard(name,time.time())
-    return 'Blackboard cleared successfully',200
+
+	if ret == 200:
+	    return 'Blackboard cleared successfully', ret
+	else:
+		return 'An error occurred', ret
 
 @app.route('/blackboard/read', methods=['GET'])         #read Data from the blackboard
 def readBlackboard():
     #parameters
     name = request.args.get('name')
-    return 'Blackboard read successfully',200
+
+	if ret == 200:
+	    return 'Blackboard read successfully', ret
+	else:
+		return 'An error occurred', ret
 
 @app.route('/blackboard/getStatus', methods=['GET'])    #return the state from the blackboard
 def getBlackboardStatus():
@@ -54,23 +68,38 @@ def getBlackboardStatus():
 
     ret = d.get_blackboard_status(name)
 
-    return 'GET_BLACKBOARD_STATUS',200
+	if ret == 200:
+	    return 'GET_BLACKBOARD_STATUS', ret
+	else:
+		return 'An error occurred', ret
 
 @app.route('/blackboard/list', methods=['GET'])         #get all blackboardnames
 def listBlackboard():
     ret = d.list_blackboards()
-    return 'List_BLACKBOARD',200
+
+	if ret == 200:
+	    return 'List_BLACKBOARD', ret
+	else:
+		return 'An error occurred', ret
 
 @app.route('/blackboard/delete',methods=['DELETE'])     #delete current blacklist
 def deleteBlackboard():
     #parameters
     name = request.args.get('name')
     ret = d.delete_blackboard(name)
-    return 'DELETE_BLACKBOARD',200
+
+	if ret == 200:
+	    return 'DELETE_BLACKBOARD', ret
+	else:
+		return 'An error occurred', ret
 
 @app.route('/blackboard/deleteAll', methods=['DELETE'])   #delete all blacklists
 def deleteAllBlackboards():
     ret = d.delete_all_blackboards()
-    return 'DELETE_ALL_BLACKBOARDS',200
+
+	if ret == 200:
+	    return 'DELETE_ALL_BLACKBOARDS', ret
+	else:
+		return 'An error occurred', ret
 
 app.run()

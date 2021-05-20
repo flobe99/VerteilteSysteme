d0ef2f2bf04bdb1f6bf9b7c99bc779d173fec060
flobe99 <60112645+flobe99@users.noreply.github.com>
2021-05-20 11:30:05 +0200

    init Python Flask
    add routes

--- server/server.py
+++ server/server.py
@@ -6,23 +6,39 @@ app = Flask(__name__)
 def welcome():
     return 'Welcome'
 
-@app.route('/createBlackboard')
+@app.route('/blackboard')
+def welcome():
+    return 'Welcome'
+
+@app.route('/blackboard/create')
 def createBlackboard():
     return 'Creating blackboard'
 
-@app.route('/displayBlackboard')
+@app.route('/blackboard/display')
 def displayBlackboard():
-    return 'DISPLAY_BLACKBOARD '
+    return 'DISPLAY_BLACKBOARD'
 
-@app.route('/clearBlackboard')
+@app.route('/blackboard/clear')
 def clearBlackboard():
     return 'CLEAR_BLACKBOARD '
 
-@app.route('/deleteBlackboard')
+@app.route('/blackboard/read')
+def clearBlackboard():
+    return 'READ_BLACKBOARD '
+
+@app.route('/blackboard/getStatus')
+def clearBlackboard():
+    return 'GET_BLACKBOARD_STATUS'
+
+@app.route('/blackboard/list')
+def clearBlackboard():
+    return 'List_BLACKBOARD '
+
+@app.route('/blackboard/delete')
 def deleteBlackboard():
     return 'DELETE_BLACKBOARD '
 
-@app.route('/deleteAllBlackboards')
+@app.route('/blackboard/deleteAll')
 def deleteAllBlackboards():
     return 'DELETE_ALL_BLACKBOARDS  '
 

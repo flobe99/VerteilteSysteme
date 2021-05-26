f85549433e8caab1522db9510b272214765c4bf2
bastschion66 <60140196+bastschion66@users.noreply.github.com>
2021-05-26 12:40:50 +0200

    Update database.py

--- server/database.py
+++ server/database.py
@@ -7,19 +7,23 @@ class Database(object):
 		self.db = self.client["blackboard"]
 		self.collection = self.db["blackboard1"]
 
-	def validate_blackboard(self, name, validityTime, timestamp):
-	#Überprüft den Datensatz auf Gültigkeit der Zeit
+	def validate_blackboard(self, name, content, validityTime, timestamp):
+	# Überprüft den Datensatz auf Gültigkeit der Zeit
+	# Dabei wird die aktuelle Zeit mit der gespeicherten Zeit + die Gültigkeitsdauer verglichen.
 		now = datetime.now()
 		time = timestamp + timedelta(seconds = validityTime)
 		if validityTime == 0:
 			self.collection.update_one({"name": name}, {"$set":{"validity": "true"}})
 		elif now < time:
 			self.collection.update_one({"name": name}, {"$set":{"validity": "true"}})
+		elif content == "":
+			self.collection.update_one({"name": name}, {"$set":{"validity": "true"}})
 		else:
 			self.collection.update_one({"name": name}, {"$set":{"validity": "false"}})
 
 	def create_blackboard(self, name, validityTime):
-	#Erstellt auf dem Server eine neues leeres Blackboard
+	# Erstellt auf dem Server eine neues leeres Blackboard
+	# Zu Beginn werden die Attribute name, validityTime, und timestamp angelegt
 		try:
 			if self.collection.count_documents({"name": name}) > 0:
 				return (None, 409)
@@ -31,11 +35,11 @@ class Database(object):
 			return (None, 500)
 
 	def display_blackboard(self, name, content):
-	#Aktualisiert den Inhalt eines Blackboards. Im gleichen Zuge wird die Aktualitätsinformation (Zeitstempel) aktualisiert
+	# Aktualisiert den Inhalt eines Blackboards. Im gleichen Zuge wird die Aktualitätsinformation (Zeitstempel) aktualisiert
 		try:
 			if self.collection.count_documents({"name": name}) > 0:
 				time = datetime.now()
-				self.collection.update_one({"name": name}, {"$set":{"content": content, "validity": "true", "timestamp": time}})
+				self.collection.update_one({"name": name}, {"$set":{"content": content, "timestamp": time}})
 				return (None, 200)
 			else:
 				return (None, 404)
@@ -43,10 +47,10 @@ class Database(object):
 			return (None, 500)
 
 	def clear_blackboard(self, name):
-	#Löscht den Inhalt eines Blackboards. Blackboard ist nicht mehr gültig
+	# Löscht den Inhalt eines Blackboards. Blackboard ist nicht mehr gültig
 		try:
 			if self.collection.count_documents({"name": name}) > 0:
-				self.collection.update_one({"name": name}, {"$set": {"content": "", "validity": "false"}})
+				self.collection.update_one({"name": name}, {"$set": {"content": ""}})
 				return (None, 200)
 			else:
 				return (None, 404)        
@@ -54,14 +58,16 @@ class Database(object):
 			return (None, 500)
 
 	def read_blackboard(self, name):
-	#Ließt den Inhalt eines Blackboards aus. Zusätlich wird die Gültigkeit der Daten signalisiert. Wenn die Nachricht veraltet ist wird diese Information zurück gegeben
+	# Ließt den Inhalt eines Blackboards aus. Zusätlich wird die Gültigkeit der Daten signalisiert. Wenn die Nachricht veraltet ist wird diese Information zurück gegeben.
+	# Die Gültigkeit der Daten wird als eigenes Attribut in der Funktion validate_blackboard übergeben.
 		try:
 			temp = self.collection.find({"name": name},{'_id': False})
 			if temp is not None:	
 				name = temp[0]["name"]
 				validityTime = temp[0]["validityTime"]
 				timestamp =	temp[0]["timestamp"]
-				self.validate_blackboard(name, validityTime, timestamp)
+				content = temp[0]["content"]
+				self.validate_blackboard(name, content, validityTime, timestamp)
 				results = self.collection.find({"name": name},{'_id': False})
 				return (results[0], 200)
 			else:
@@ -71,7 +77,7 @@ class Database(object):
 
 
 	def get_blackboard_status(self, name):
-	#Gibt den aktuellen Status eines Blackboards zurück
+	# Gibt den aktuellen Status eines Blackboards zurück.
 		try:
 			temp = self.collection.find({"name": name},{'_id': False})
 			if temp is not None:	
@@ -87,7 +93,7 @@ class Database(object):
 			return (None, 500)
 
 	def list_blackboards(self):
-	#Listet alle vorhandenen Blackboards auf
+	# Listet alle vorhandenen Blackboards auf
 		try:
 			results = list(self.collection.find({},{'_id': False}))
 			return (results, 200)
@@ -95,7 +101,7 @@ class Database(object):
 			return (None, 500)
 
 	def delete_blackboard(self, name):
-	#Löscht ein Blackboard
+	# Löscht ein Blackboard
 		try:
 			if self.collection.count_documents({"name": name}) > 0:
 				self.collection.delete_one({"name": name})
@@ -107,21 +113,9 @@ class Database(object):
 
 
 	def delete_all_blackboards(self):
-	#Löscht alle Blackboards
+	# Löscht alle Blackboards
 		try:
 			self.collection.delete_many({})
 			return (None, 200)
 		except:
 			return (None,500)
-		
-	def validate_blackboard(self, validityTime, timestamp):
-		now = datetime.now() # current date and time
-		time = timestamp + timedelta(seconds = validityTime)
-		if validityTime == 0:
-			return True
-		elif now < time:
-			return True
-		else:
-			return False
-
-	

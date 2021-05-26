a8dac032118f06fdd563efdeef18aa1ca57d03a1
bastschion66 <60140196+bastschion66@users.noreply.github.com>
2021-05-26 12:18:17 +0200

    Update database.py

--- server/database.py
+++ server/database.py
@@ -1,4 +1,5 @@
 import pymongo
+from datetime import datetime, timedelta
 
 class Database(object):
 	def __init__(self):
@@ -6,33 +7,47 @@ class Database(object):
 		self.db = self.client["blackboard"]
 		self.collection = self.db["blackboard1"]
 
-	def create_blackboard(self, name, validity, time):
+	def validate_blackboard(self, name, validityTime, timestamp):
+	#Überprüft den Datensatz auf Gültigkeit der Zeit
+		now = datetime.now()
+		time = timestamp + timedelta(seconds = validityTime)
+		if validityTime == 0:
+			self.collection.update_one({"name": name}, {"$set":{"validity": "true"}})
+		elif now < time:
+			self.collection.update_one({"name": name}, {"$set":{"validity": "true"}})
+		else:
+			self.collection.update_one({"name": name}, {"$set":{"validity": "false"}})
+
+	def create_blackboard(self, name, validityTime):
 	#Erstellt auf dem Server eine neues leeres Blackboard
 		try:
 			if self.collection.count_documents({"name": name}) > 0:
 				return (None, 409)
 			else:
-				self.collection.insert_one({"name": name, "validity": validity, "time": time})
+				time = validityTime
+				now = datetime.now()
+				self.collection.insert_one({"name": name, "validityTime": time, "timestamp": now})
 				return (None, 200)
 		except:
 			return (None, 500)
 
-	def display_blackboard(self, name, text, validity, time):
+	def display_blackboard(self, name, content):
 	#Aktualisiert den Inhalt eines Blackboards. Im gleichen Zuge wird die Aktualitätsinformation (Zeitstempel) aktualisiert
 		try:
 			if self.collection.count_documents({"name": name}) > 0:
-				self.collection.update_one({"name": name}, {"$set":{"text": text, "validity": validity, "time": time}})
+				time = datetime.now()
+				self.collection.update_one({"name": name}, {"$set":{"content": content, "validity": "true", "timestamp": time}})
 				return (None, 200)
 			else:
 				return (None, 404)
 		except:
 			return (None, 500)
 
-	def clear_blackboard(self, name, time):
+	def clear_blackboard(self, name):
 	#Löscht den Inhalt eines Blackboards. Blackboard ist nicht mehr gültig
 		try:
 			if self.collection.count_documents({"name": name}) > 0:
-				self.collection.update_one({"name": name}, {"$set": {"text": "", "validity": "false", "time": time}})
+				self.collection.update_one({"name": name}, {"$set": {"content": "", "validity": "false"}})
 				return (None, 200)
 			else:
 				return (None, 404)        
@@ -42,8 +57,13 @@ class Database(object):
 	def read_blackboard(self, name):
 	#Ließt den Inhalt eines Blackboards aus. Zusätlich wird die Gültigkeit der Daten signalisiert. Wenn die Nachricht veraltet ist wird diese Information zurück gegeben
 		try:
-			results = self.collection.find({"name": name},{'_id': False})
-			if results is not None:
+			temp = self.collection.find({"name": name},{'_id': False})
+			if temp is not None:	
+				name = temp[0]["name"]
+				validityTime = temp[0]["validityTime"]
+				timestamp =	temp[0]["timestamp"]
+				self.validate_blackboard(name, validityTime, timestamp)
+				results = self.collection.find({"name": name},{'_id': False})
 				return (results[0], 200)
 			else:
 				return (None, 404)
@@ -54,8 +74,13 @@ class Database(object):
 	def get_blackboard_status(self, name):
 	#Gibt den aktuellen Status eines Blackboards zurück
 		try:
-			results = self.collection.find({"name": name},{'_id': False})
-			if results is not None:
+			temp = self.collection.find({"name": name},{'_id': False})
+			if temp is not None:	
+				name = temp[0]["name"]
+				validityTime = temp[0]["validityTime"]
+				timestamp =	temp[0]["timestamp"]
+				self.validate_blackboard(name, validityTime, timestamp)
+				results = self.collection.find({"name": name},{'_id': False})
 				return (results[0], 200)
 			else:
 				return (None, 404)
@@ -89,3 +114,15 @@ class Database(object):
 			return (None, 200)
 		except:
 			return (None,500)
+		
+	def validate_blackboard(self, validityTime, timestamp):
+		now = datetime.now() # current date and time
+		time = timestamp + timedelta(seconds = validityTime)
+		if validityTime == 0:
+			return True
+		elif now < time:
+			return True
+		else:
+			return False
+
+	
